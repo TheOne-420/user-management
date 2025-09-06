c9929b420716239c23909b4df91e2e87556cd42c
Your Name <tabishsaiyedsahab@gmail.com>
2025-09-06 12:24:09 +0530

    add: jest

---
+++ src/app/form/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Loading() {
+    return (
+        <h2 className='font-extrabold text-4xl absolute grid place-self-center place-content-center text-center'>
+           Loading... 
+        </h2>
+    );
+}
+
+export default Loading;
---
+++ src/app/table/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Loading() {
+    return (
+        <h1 className='font-extrabold text-4xl absolute grid place-self-center place-content-center text-center'>
+           Loading... 
+        </h1>
+    );
+}
+
+export default Loading;
